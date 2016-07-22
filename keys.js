6952ae939d41ea8a7055b55b58bee63508cc754b
FrankBeatrice <Frank.Beatrice@rutgers.edu>
2016-07-22 15:47:43 -0400

    Add files via upload

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log('this is loaded');
+
+//get your API creds by following these steps:
+//Step One: https://apps.twitter.com/app/new
+//Step Two: use http:// for your urls
+//Step Three: then go to Keys and Access Tokens to get your credentials for below
+//Step Four: then you have to click the button below on that page to create an access token
+exports.twitterKeys = {
+  consumer_key: 'hv61PQ1nUnStIP4lxGg5dNWSz',
+  consumer_secret: 'ZhRyvn7GZeUtWNY1uEm1KkmwK3Q5tIJp13fA9oN9j5oimzywDE',
+  access_token_key: ' 1532446794-uQGdVDwv1iBaSpTTloJKnf7zNmIvULe2vGFzU6E',
+  access_token_secret: 'xBnLYfHgYZAUN4plhl8nL39mNWFBo9asnrMUQBHxLVy6x',
+}
